34a7a19399542034b27aa215fbf4500d4f1dbe94
Vito <zhangwei.noair@gmail.com>
2013-09-23 17:56:14 +0800

    Initial Commit

---
+++ VIOpenWeatherConditionDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VIOpenWeatherConditionDemo
+//
+//  Created by Vito on 13-9-23.
+//  Copyright (c) 2013年 Vito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
